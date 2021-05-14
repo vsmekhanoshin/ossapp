d2a67bc3654c9649313e08a4cce695f8a5082238
Roman Dubchak <dubrathers@mail.ru>
2021-05-14 23:21:53 +0300

    success test Style Controller

--- src/main/java/com/ossapp/mainapp/entities/UserStylePK.java
+++ src/main/java/com/ossapp/mainapp/entities/UserStylePK.java
@@ -1,18 +1,18 @@
-package com.ossapp.mainapp.entities;
-
-import lombok.Data;
-
-import javax.persistence.Column;
-import javax.persistence.Embeddable;
-import java.io.Serializable;
-
-@Embeddable
-@Data
-public class UserStylePK implements Serializable {
-
-    @Column(name = "user_id")
-    private Long userId;
-
-    @Column(name = "style_id")
-    private Long styleId;
-}
+//package com.ossapp.mainapp.entities;
+//
+//import lombok.Data;
+//
+//import javax.persistence.Column;
+//import javax.persistence.Embeddable;
+//import java.io.Serializable;
+//
+//@Embeddable
+//@Data
+//public class UserStylePK implements Serializable {
+//
+//    @Column(name = "user_id")
+//    private Long userId;
+//
+//    @Column(name = "style_id")
+//    private Long styleId;
+//}
