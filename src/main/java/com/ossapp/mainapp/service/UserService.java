2e4b0b8d4415ea8e9bc6d9ee2c700647ed92671e
vsmekhanoshin <vsmehanosin@gmail.com>
2021-05-16 14:04:24 +0500

    User data access layer created

---
+++ src/main/java/com/ossapp/mainapp/service/UserService.java
@@ -0,0 +1,22 @@
+package com.ossapp.mainapp.service;
+
+import com.ossapp.mainapp.entities.User;
+import com.ossapp.mainapp.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+}
