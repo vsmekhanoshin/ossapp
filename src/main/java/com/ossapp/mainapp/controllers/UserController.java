265eb00704fa47075f510491129dbddd098b7d79
vsmekhanoshin <vsmehanosin@gmail.com>
2021-05-19 21:36:37 +0500

    UserController itself

--- src/main/java/com/ossapp/mainapp/controllers/UserController.java
+++ src/main/java/com/ossapp/mainapp/controllers/UserController.java
@@ -3,25 +3,31 @@ package com.ossapp.mainapp.controllers;
 import com.ossapp.mainapp.entities.User;
 import com.ossapp.mainapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.PostConstruct;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
-    private UserService userservice;
+    private final UserService userservice;
 
-    @Autowired
-    public UserController(UserService userService) {
-        this.userservice = userService;
+    public UserController(UserService userServiceImpl) {
+        this.userservice = userServiceImpl;
     }
 
     @GetMapping
     public List<User> getAllUsers(){
         return userservice.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable("id") Long id){
+        return new ResponseEntity<>(userservice.findById(id), HttpStatus.OK);
     }
 }
