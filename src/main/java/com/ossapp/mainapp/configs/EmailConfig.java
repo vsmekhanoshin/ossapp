7de4c491f6f3c811ee56f7e89713e6442eb4883b
vsmekhanoshin <vsmehanosin@gmail.com>
2021-07-23 16:23:32 +0500

    EmailConfig created

---
+++ src/main/java/com/ossapp/mainapp/configs/EmailConfig.java
@@ -0,0 +1,36 @@
+package com.ossapp.mainapp.configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class EmailConfig {
+    @Value("${spring.mail.username: mailforossapp@gmail.com}")
+    private String username;
+
+    @Value("${spring.mail.password: 1234oss*}")
+    private String password;
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+
+        Properties properties = mailSender.getJavaMailProperties();
+        properties.put("mail.transport.protocol", "smtp");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.ssl.enable", "true");
+        properties.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
