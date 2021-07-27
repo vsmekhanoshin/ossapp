35f96e0cc5e0e126eaf96aa31b4a10654af49b07
Roman <dubrathers@mail.ru>
2021-07-27 14:34:20 +0300

    upd project

--- src/main/java/com/ossapp/mainapp/service/UserService.java
+++ src/main/java/com/ossapp/mainapp/service/UserService.java
@@ -1,6 +1,7 @@
 package com.ossapp.mainapp.service;
 
 import com.ossapp.mainapp.dto.RequestUserDto;
+import com.ossapp.mainapp.dto.ResponseProfileUserDto;
 import com.ossapp.mainapp.dto.ResponseUserDto;
 import com.ossapp.mainapp.entities.User;
 import org.springframework.data.jpa.domain.Specification;
@@ -13,6 +14,8 @@ public interface UserService {
     User save(RequestUserDto requestUserDto);
 
     ResponseUserDto findById(Long id);
+
+    ResponseProfileUserDto findMyProfileById(Long id);
 
     Long findCityIdByUserId(Long userId);
 
